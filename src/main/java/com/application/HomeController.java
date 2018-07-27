25823d22a23a8470c65613d59ac7c9c319df0032
PERSISTENT\prateek_shrivastava <24779354+shrivastava-prateek@users.noreply.github.com>
2018-07-27 20:01:41 +0530

    updated

--- src/main/java/com/application/HomeController.java
+++ src/main/java/com/application/HomeController.java
@@ -1,10 +1,13 @@
 package com.application;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/home-controller")
 public class HomeController {
  
@@ -14,10 +17,22 @@ public class HomeController {
     	System.out.println("Hit");
     }
     
+    @GetMapping("/getString")
+    public String returnString() {
+        return "My name is Prateek";
+    }
     
-    @RequestMapping("/")
+    @GetMapping("/getIntger")
+    public Integer returnInteger() {
+        return 99999999;
+    }
+    
+    
+    @GetMapping("/getList")
     public List<String> returnList() {
-        
-     return null;
+        List<String> strList = new ArrayList<>();
+        strList.add("Prateek");
+        strList.add("Shrivastava");
+        return strList;
     }
 }
